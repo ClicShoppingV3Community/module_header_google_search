53ee234458da970208677878f69c486962223dda
ClicShopping <Yae8mr2o*>
2020-08-12 10:42:56 +0200

    little improvment

--- sources/template/Default/modules/modules_header/he_header_google_search.php
+++ sources/template/Default/modules/modules_header/he_header_google_search.php
@@ -26,7 +26,7 @@
       $this->title = CLICSHOPPING::getDef('module_header_google_search_title');
       $this->description = CLICSHOPPING::getDef('module_header_google_search_description');
 
-      if ( defined('MODULE_HEADER_GOOGLE_SEARCH_STATUS') ) {
+      if (defined('MODULE_HEADER_GOOGLE_SEARCH_STATUS')) {
         $this->sort_order = MODULE_HEADER_GOOGLE_SEARCH_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_GOOGLE_SEARCH_STATUS == 'True');
       }
